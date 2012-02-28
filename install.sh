dfca34d05e429b31a3a7d495e8f813138329ab9f
hatone <b1006006@gmail.com>
2012-02-27 18:09:43 -0800

    add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+cd /tmp
+git clone git@github.com:hatone/WordPress-for-rackhub.git
+sudo chown rackhuber www
+cp -r WordPress-for-rackhub/. /var/www/.
+
+sudo service apache2 start
+sudo service mysql.server start
