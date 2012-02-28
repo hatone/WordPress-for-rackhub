64037ab675791a77152dfa9e561433ee0c2bc217
hatone <b1006006@gmail.com>
2012-02-27 18:16:42 -0800

    add

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@ cd /tmp
 git clone git@github.com:hatone/WordPress-for-rackhub.git
 sudo chown rackhuber www
 cp -r WordPress-for-rackhub/. /var/www/.
-
 sudo service apache2 start
 sudo service mysql.server start
